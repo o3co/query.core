d98a918406ce8afaa56632673151b4fc51999298
Yoshi Aoki <yoshi@44services.jp>
2015-03-27 00:18:57 +0900

    add Exceptions

---
+++ Exception/ParserException.php
@@ -0,0 +1,15 @@
+<?php
+namespace O3Co\Query\Exception;
+
+/**
+ * ParserException 
+ * 
+ * @package { PACKAGE }
+ * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
+ * @author Yoshi<yoshi@1o1.co.jp> 
+ * @license { LICENSE }
+ */
+class ParserException extends \RuntimeException 
+{
+}
+
