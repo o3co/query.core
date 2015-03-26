95ce5f550c822c0b5a72fb814d33edc2c293716c
Yoshi Aoki <yoshi@44services.jp>
2015-03-26 16:45:40 +0900

    add Offset and Limit Clause

--- Query/Term/LimitClause.php
+++ Query/Term/LimitClause.php
@@ -1,17 +1,18 @@
 <?php
 namespace O3Co\Query\Query\Term;
 
-use O3Co\Query\Query\Term;
-
 /**
- * Clause 
+ * LimitClause 
  * 
+ * @uses ValueExpression
+ * @uses Clause
  * @package { PACKAGE }
  * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
  * @author Yoshi<yoshi@1o1.co.jp> 
  * @license { LICENSE }
  */
-interface Clause
+class LimitClause extends ValueExpression implements Clause
 {
+
 }
- 
+
--- Query/Term/OffsetClause.php
+++ Query/Term/OffsetClause.php
@@ -1,17 +1,18 @@
 <?php
 namespace O3Co\Query\Query\Term;
 
-use O3Co\Query\Query\Term;
-
 /**
- * Clause 
+ * OffsetClause 
  * 
+ * @uses ValueExpression
+ * @uses Clause
  * @package { PACKAGE }
  * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
  * @author Yoshi<yoshi@1o1.co.jp> 
  * @license { LICENSE }
  */
-interface Clause
+class OffsetClause extends ValueExpression implements Clause
 {
+
 }
- 
+
